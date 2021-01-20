1452d73382fff0242e750d27a52cf53199f738a7
LAB113 <LAB113@INFO.RN>
2021-01-20 10:03:11 +0100

    ok

---
+++ Paz.Eyal.4h.MediaMobile/Paz.Eyal.4h.MediaMobile/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Paz.Eyal._4h.MediaMobile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Campione[] c = new Campione[10]; // vettore
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+  
+    public class Campione
+    {
+        //variabili
+
+        private string _data;
+        private int _umidita; // si misura in percentuale
+        private double _temperatura; // si misura in gradi
+        private int _pressione; //si misura in millibar
+
+        // property
+        public string Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+            }
+
+        }
+        public int Umidita
+        {
+            get
+            {
+                return _umidita;
+            }
+            set
+            {
+                _umidita = value;
+            }
+
+        }
+        public double Temperatura
+        {
+            get
+            {
+                return _temperatura;
+            }
+            set
+            {
+                _temperatura=value;
+            }
+
+        }
+        public int Presssione
+        {
+            get
+            {
+                return _pressione;
+            }
+            set
+            {
+                _pressione=value;
+            }
+
+        }
+    }
+}
+
